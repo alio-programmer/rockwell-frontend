1a6fe5be5548fd7e0aea81c6deae262ddc78e3f9
Gaurav Singh Bisht <92017948+alio-programmer@users.noreply.github.com>
2025-03-21 22:52:58 +0530

    Update Header.jsx

--- src/components/partials/Header.jsx
+++ src/components/partials/Header.jsx
@@ -37,7 +37,7 @@ const Header = ({ setOpen, open }) => {
   const logout = async () => {
     // setUserInfo(null)
     // navigate('/login')
-    const response = await axios.get("http://localhost:8000/auth/logout", {
+    const response = await axios.get("https://rockwell-project.onrender.com", {
       withCredentials: true,
       credentials: "include",
     });
